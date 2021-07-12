ca1f87cbd07ec00b9e8c401fc02590ce85c7f33b
dope <dope0814@gmail.com>
2021-07-12 11:39:23 +0900

    3.3 Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 class App extends React.Component{
-   return (
-     <div>
-      
-     </div>
-   )
+  state = {
+    isLoading : true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "we are ready"} </div>;
+  }
 }
 
 export default App;
